5f15af26db288bb9042b814365ec838281c09876
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-10 02:57:40 +0530

    Auto Deploy Using Github Is Now Working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
+        <p className="App-header">Something Special Coming Soon ..................</p>
       </header>
     </div>
   );
