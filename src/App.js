518d4420c615aa834ef5cd041225fd534c6979c4
SANSKAR KABRA <sanskarkabra1306@gmail.com>
2020-12-10 17:29:15 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" /> 
-        <p className="App-header">Something Cool Coming Soon ..................</p>
+        <p className="App-header">Something Cool Coming Soon </p>
       </header>
     </div>
   );
