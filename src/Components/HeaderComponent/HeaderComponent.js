c4f6449e2fcb58230113c5ae5965b2ef3b5e9012
sanskar1306 <sanskarkabra1306@gmail.com>
2020-12-14 06:15:01 +0530

    Drawer updated

--- src/Components/HeaderComponent/HeaderComponent.js
+++ src/Components/HeaderComponent/HeaderComponent.js
@@ -11,24 +11,27 @@ import MenuIcon from '@material-ui/icons/Menu';
 import {Hidden} from '@material-ui/core'
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-
-import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import List from '@material-ui/core/List';
+
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-
+import CancelRoundedIcon from '@material-ui/icons/CancelRounded';
 import clsx from 'clsx';
-
-
-
+import LockOpenRoundedIcon from '@material-ui/icons/LockOpenRounded';
+import PersonAddRoundedIcon from '@material-ui/icons/PersonAddRounded';
+import Paper from '@material-ui/core/Paper';
+import Slide from '@material-ui/core/Slide';
+import Switch from '@material-ui/core/Switch';
 function HeaderComponent(){
   
         const useStyles = makeStyles((theme) => ({
           list: {
-            width: 300,
+           
+            
           },
           fullList: {
             width: 'auto',
@@ -80,42 +83,60 @@ function HeaderComponent(){
                 
                   <React.Fragment >
                     
-                    <SwipeableDrawer anchor="left" open={state} onClose={toggleDrawer( false)} onOpen={toggleDrawer(true)} 
-                     transitionDuration={ 10 }	>
-                      <div style={{height:200,width:300,background:'red'}}>
+                    <SwipeableDrawer anchor="left" open={state} onClose={toggleDrawer( false)}  onOpen={toggleDrawer(true)} >
+                      {/* <div style={{height:200,width:300,background:'red'}}>
                         <span style={{fontSize:30,fontWeight:'bold',color:'white',}}>
                           HEY USER
                         </span>
                        
-                      </div>
+                      </div> */}
                       <List className={classes.list}>
+                      <ListItem  key="Cancel" onClick={toggleDrawer(false)} style={{position:'absoloute',right:0}}>
+                      
+                      <ListItemIcon> <CancelRoundedIcon color="secondary" /> </ListItemIcon>
+                      
+                      </ListItem>
                       <Link to="/"> 
-                      <ListItem button key="Home">
-                      <ListItemIcon> <HomeRoundedIcon /> </ListItemIcon>
-                      <ListItemText primary="Home" />
+                      <ListItem button key="Home" onClick={toggleDrawer(false)}>
+                      <ListItemIcon  > <HomeRoundedIcon color="primary" /> </ListItemIcon>
+                      <ListItemText primary="HOME" />
                       </ListItem>
                       </Link>
-
+                      <Divider />
                       <Link to="/Search"> 
-                      <ListItem button key="Search">
-                      <ListItemIcon> <SearchRoundedIcon /> </ListItemIcon>
-                      <ListItemText primary="Search" />
+                      <ListItem button key="Search" onClick={toggleDrawer(false)}>
+                      <ListItemIcon> <SearchRoundedIcon color="primary"/> </ListItemIcon>
+                      <ListItemText primary="SEARCH" />
                       </ListItem>
                       </Link>
-
+                      <Divider />
                       <Link to="/dashboard"> 
-                      <ListItem button key="Dashboard">
-                      <ListItemIcon> <DashboardRoundedIcon /> </ListItemIcon>
-                      <ListItemText primary="Dashboard" />
+                      <ListItem button key="Dashboard" onClick={toggleDrawer(false)}>
+                      <ListItemIcon> <DashboardRoundedIcon color="primary" /> </ListItemIcon>
+                      <ListItemText primary="DASHBOARD" />
                       </ListItem>
                       </Link>
-                      <ListItem button key="Login">
-                      <Button variant="contained" color="secondary" href="/login" className={classes.Button1}> Login</Button>
+                      <Divider />
+                      <ListItem  key="Dark Mode" >
+                      <ListItemIcon> <Brightness4Icon color="primary"/> </ListItemIcon>
+                      {/* <ListItemText primary="DARK MODE" /> */}
+                      <Switch />
                       </ListItem>
-                      <ListItem button key="Signup">
-                      <Button variant="contained" color="primary" href="/signup" className={classes.Button1}> Signup</Button>
+                      <Divider />
+                      <Link to="/login">
+                      <ListItem button key="Login" onClick={toggleDrawer(false)}>
+                      <ListItemIcon> <LockOpenRoundedIcon color="primary" /> </ListItemIcon>
+                      <ListItemText primary="LOGIN" />
                       </ListItem>
-                    
+                      </Link>
+                      <Divider />
+                      <Link to="/signup">
+                      <ListItem button key="Signup" onClick={toggleDrawer(false)}>
+                      <ListItemIcon> <PersonAddRoundedIcon color="primary"/> </ListItemIcon>
+                      <ListItemText primary="SIGNUP" />
+                      </ListItem>
+                      </Link>
+                      <Divider />
                     
                       </List>
                     </SwipeableDrawer>
