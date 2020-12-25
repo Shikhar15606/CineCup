1765f26d009612e9fe83ed5a513558f611ad2cd1
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-25 18:43:01 +0530

    LeaderBoard Bug Fixed

--- src/action/movie_actions.js
+++ src/action/movie_actions.js
@@ -24,17 +24,28 @@ export const fetchMoviesData = () => {
                 arr.push({id:doc.data().MovieId, votes:doc.data().Votes, rank:i})
                 i++;
             });
-            let result = [];
-            arr.forEach(element => {
-            axios(`https://api.themoviedb.org/3/movie/${element.id}?api_key=${TMDB_API_KEY}`)
-            .then((res) => {
-                result.push({...res.data, rank:element.rank, votes:element.votes});  
-            })
-            })
-            dispatch({
-                type:FETCH_MOVIES_DATA_SUCCESS,
-                payload:result
+            console.log(arr.length)
+            xyz(arr)
+            .then((result)=>{
+                dispatch({
+                    type:FETCH_MOVIES_DATA_SUCCESS,
+                    payload:result
+                })
             })
         })
     }
+}
+
+async function xyz (arr){
+    let r = [];
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        let res = await axios(`https://api.themoviedb.org/3/movie/${element.id}?api_key=${TMDB_API_KEY}`)
+        r.push({...res.data, rank:element.rank, votes:element.votes}); 
+            if(i === arr.length - 1)
+            {
+                console.log("BEEP Beep Beep",r.length,"BEEP Beep Beep");
+                return r;
+            } 
+      }
 }
