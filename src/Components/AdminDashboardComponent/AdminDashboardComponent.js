291247946d38769e06faf0fbe2d7793fcfb22d93
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-28 14:52:32 +0530

    Adding and removing admins

--- src/Components/AdminDashboardComponent/AdminDashboardComponent.js
+++ src/Components/AdminDashboardComponent/AdminDashboardComponent.js
@@ -11,6 +11,7 @@ import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import firebase from 'firebase';
 
 const AdminDashboardComponent = () => {
     const dispatch = useDispatch();
@@ -93,6 +94,41 @@ const AdminDashboardComponent = () => {
         }
     },[user.isLoggedIn])
 
+    // ======================== Fetching other users =================================
+    const [allusers,setallusers] = useState([]);    
+    const [alladmin,setalladmin] = useState([]);
+    useEffect(()=>{
+      const db = firebase.firestore();
+      let unsubscribe = db.collection('users')
+      .onSnapshot(function(querySnapshot){
+        let arr = [];
+        let arr1 = [];
+        querySnapshot.forEach((doc)=>{
+          if(doc.data().IsAdmin)
+            arr1.push(doc.data());
+          else
+            arr.push(doc.data())
+        })
+        setallusers(arr);
+        setalladmin(arr1);
+      })
+    },[])
+
+    function makeAdmin(emailid,e){
+      e.preventDefault()
+      const db = firebase.firestore();
+      db.collection("users").doc(emailid)
+      .update({IsAdmin:true})
+    }
+
+    function removeFromAdmin(emailid,e){
+      e.preventDefault();
+      const db = firebase.firestore();
+      db.collection("users").doc(emailid)
+      .update({IsAdmin:false})
+    }
+
+
   // =========================== Main Return from this component ==================================
   if(user.isLoading)
     return(
@@ -105,8 +141,6 @@ const AdminDashboardComponent = () => {
       </div>
       <main>
       <section className="wrapper1">
-          {
-            console.log(result,result.length)}
           {  
             result.length !== 0 ?
             (
@@ -136,7 +170,73 @@ const AdminDashboardComponent = () => {
         :
         <div></div>
         }
+      {/* Admin */}
+      <div style={{marginTop:100}}>
+        <h1 style={{color:"white"}}>All Administrators</h1>
+      </div>
+      <main>
+          {  
+            alladmin.length !== 0 ?
+            (
+              <section className="wrapper1">
+              {alladmin.map((admin) => (              
+                <React.Fragment className="profile-card">
+                      <div class="our-team" style={{width:"250px",height:"220px"}}>
+                        <div class="picture">
+                          <img style={{width:"130px",height:"130px"}} src={admin.ProfilePic}/>
+                        </div>
+                        <div class="team-content">
+                          <h3 class="name" style={{color:"black"}}>{admin.Name}</h3>
+                          <h4 class="title">{admin.Email}</h4>
+                        </div>
+                        <ul class="social">
+                        <Button variant="contained" style={{width:"100%", backgroundColor:" #1369ce"}} onClick={(e) => {removeFromAdmin(admin.Email,e)}} endIcon={<LocalMoviesIcon />} className="but1">
+                        Dismiss From Admin</Button>
+                        </ul>
+                      </div>
+                </React.Fragment>
+              ))}
+              </section>
+            )
+            :(<p>
+                   There are no admin users
+             </p>)
+          }
       </main>
+      {/* Users */}
+      </main>
+      <div style={{marginTop:100}}>
+        <h1 style={{color:"white"}}>All users</h1>
+      </div>
+      <main>
+          {  
+            allusers.length !== 0 ?
+            (
+              <section className="wrapper1">
+              {allusers.map((user) => (              
+                <React.Fragment className="profile-card">
+                      <div class="our-team" style={{width:"250px",height:"220px"}}>
+                        <div class="picture">
+                          <img style={{width:"130px",height:"130px"}} src={user.ProfilePic}/>
+                        </div>
+                        <div class="team-content">
+                          <h3 class="name" style={{color:"black"}}>{user.Name}</h3>
+                          <h4 class="title">{user.Email}</h4>
+                        </div>
+                        <ul class="social">
+                        <Button variant="contained" style={{width:"100%", backgroundColor:" #1369ce"}} onClick={(e) => {makeAdmin(user.Email,e)}} endIcon={<LocalMoviesIcon />} className="but1">
+                        Make Admin</Button>
+                        </ul>
+                      </div>
+                </React.Fragment>
+              ))}
+              </section>
+            )
+            :(<p>
+                   There are no non admin users
+             </p>)
+          }
+        </main>
     </React.Fragment>
     );
 
