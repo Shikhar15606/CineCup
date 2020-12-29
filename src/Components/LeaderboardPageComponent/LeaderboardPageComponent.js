386eb629d87fd6de19c540cb983624d33de29137
sanskar1306 <sanskarkabra1306@gmail.com>
2020-12-29 16:40:06 +0530

    cards aligned not changed yet

--- src/Components/LeaderboardPageComponent/LeaderboardPageComponent.js
+++ src/Components/LeaderboardPageComponent/LeaderboardPageComponent.js
@@ -4,13 +4,80 @@ import { Link } from 'react-router-dom';
 import Badge from '@material-ui/core/Badge';
 import {useSelector} from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    alignSelf:"center",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    margin: 0 ,
+    width: '60%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: '20%',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color:"white"
+  },
+  inputRoot: {
+    color: 'inherit',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    color:"white",
 
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '70%',
+    [theme.breakpoints.up('sm')]: {
+      width: '50ch',
+      '&:focus': {
+        width: '60ch',
+      },
+    },
+  },
+}));
 const LeaderboardPageComponent = () => {
+  const classes = useStyles();
     const user = useSelector(state => state.user);
     let result = user.movies ? user.movies : [] ;
 
     function RenderCard ({r}) {     
+      
           return(  
+            <div className="wraap">
             <Badge  anchorOrigin={{
                 vertical: 'top',
                 horizontal: 'left',
@@ -25,6 +92,7 @@ const LeaderboardPageComponent = () => {
               </ul>
             </Link>
             </Badge> 
+            </div>
           )
         }
     if(user.isLoading)
@@ -35,8 +103,39 @@ const LeaderboardPageComponent = () => {
         <div className="wrapper2">
             <header>
                 <h1>LeaderBoard</h1>
+                <div >
+            <div >
+            
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search','id':'sear'}}
+              onKeyUp={()=>{
+                  var all = document.getElementById("leaderboard_cards").getElementsByTagName("div");
+                
+                console.log(all)
+                for(var i=0;i<all.length;i++)
+                  {
+                    var txtValue = all[i].innerText;
+                    console.log(txtValue)
+                    if (txtValue.toLowerCase().includes(document.getElementById("sear").value) ) {
+                      all[i].style.display = "";
+                    } else {
+                      all[i].style.display = "none";
+                    }
+                    
+                  } 
+              }}
+              
+            />
+            </div>
             </header>
-            <div className="card__collection clear-fix">
+            
+            <div className="card__collection clear-fix" id="leaderboard_cards">
             {  
                 result.length !== 0 ?
                 (
