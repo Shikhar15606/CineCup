44c09cd96330fb53bed53d81e066788448e0aa0f
Shikhar15606 <shikharshukla15606@gmail.com>
2020-12-31 21:05:28 +0530

    Auth Done

--- src/Components/SearchPageComponent/Result.js
+++ src/Components/SearchPageComponent/Result.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {ACCESS_TOKEN} from '../../key/key'
 import {useSelector,useDispatch} from 'react-redux'
 import { Link } from 'react-router-dom';
 import {nominate} from '../../action/user_actions';
