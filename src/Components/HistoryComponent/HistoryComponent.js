0e67c552e301778d23a1d88f8c85479883eb623b
sanskar1306 <sanskarkabra1306@gmail.com>
2021-01-03 18:27:50 +0530

    table added some css needs to be changed

--- src/Components/HistoryComponent/HistoryComponent.js
+++ src/Components/HistoryComponent/HistoryComponent.js
@@ -10,7 +10,8 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+import { faArrowCircleRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const HistoryComponent = () => {
     const user = useSelector(state => state.user);
     const useStyles = makeStyles({
@@ -38,30 +39,49 @@ const HistoryComponent = () => {
       )
       return (
         <div>
-            <h1>
-                Previous Contests
-            </h1>
-            {
+            <div class="wrapper_history">
+  <div class="list">
+    <div class="list__header">
+      <h1 style={{color:"black"}}>Previous Contests</h1>
+      
+    </div>
+    <div class="list__body">
+      <table class="list__table">
+      <tr class="header_row">
+      <th class="list__cell">S.no</th>
+    <th class="list__cell">Contest</th>
+    <th class="list__cell">Start date</th>
+    <th class="list__cell">End date</th>
+    <th class="list__cell">Link</th>
+  </tr>
+      {
                 user.history && user.history.length ?
                 (
-                    user.history.map(element => {
+                    user.history.map((element,index) => {
                         return(
-                            <Card className={classes.root}>
-                            <CardContent>
-                              <Typography variant="h5" component="h2">
-                                {`${element.Name}`}
-                              </Typography>
-                              <Typography className={classes.pos} color="textSecondary">
-                                {`Start : ${element.Start}`}
-                              </Typography>
-                              <Typography className={classes.pos} color="textSecondary">
-                              {`End : ${element.End}`}
-                              </Typography>
-                            </CardContent>
-                            <CardActions>
-                              <Button size="small"><Link to={`/history/${element.contestid}`}>See Ranking</Link></Button>
-                            </CardActions>
-                          </Card>
+                                       
+       <tr class="list__row" >
+         
+          <td class="list__cell"><span class="list__value">{index+1}</span></td>
+          <td class="list__cell"><span class="list__value">{`${element.Name}`}</span></td>
+          <td class="list__cell">
+            <span class="list__value">{`${element.Start}`}</span>
+            {/* <small class="list__label">20:10 hrs</small> */}
+            </td>
+          
+          <td class="list__cell"><span class="list__value">{`${element.End}`}</span>
+          {/* <small class="list__label">23:45 hrs</small> */}
+          </td>
+         <td class="list__cell">
+         <Link to={`/history/${element.contestid}`} className="linky"> 
+         <span class="list__value"><FontAwesomeIcon icon={faArrowCircleRight} /></span>
+         </Link>
+         </td>
+        </tr>
+        
+      
+  
+
                         )
                     })
                 )
@@ -70,6 +90,12 @@ const HistoryComponent = () => {
                     No previous contests
                 </h1>
             }
+       
+        
+      </table>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
