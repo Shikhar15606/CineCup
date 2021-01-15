15c38c75835520ecdb05b03870021b22dc52d2c2
Shikhar15606 <shikharshukla15606@gmail.com>
2021-01-15 14:41:33 +0530

    DElete Review

--- src/Components/DashboardPageComponent/DashboardPageComponent.js
+++ src/Components/DashboardPageComponent/DashboardPageComponent.js
@@ -12,12 +12,76 @@ import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Rating from '@material-ui/lab/Rating';
-import ShareButton from '../shareButton'
+import ShareButton from '../shareButton';
+import firebase from 'firebase';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 function DashboardPageComponent(){
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+  const classes = useStyles();
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
   const [result,setresult] = useState([]);  
   const [open, setOpen] = useState(false);
+  const [reviewDetail,setreviewDetail] = useState([]);
+
+    async function getMovieDetails(obj){
+      let querySnapshot = obj.docs;
+      let arr = [];
+      for(let i=0;i<querySnapshot.length;i++){
+        let doc = querySnapshot[i];
+        console.log(doc);
+            try{
+                console.log(doc.data().mid);
+                let res = await axios.get(`https://api.themoviedb.org/3/movie/${doc.data().mid}?api_key=${TMDB_API_KEY}`)
+                arr.push({...res.data, review:doc.data().review, rating:doc.data().rating, reviewID:doc.id})
+            }catch(err){
+                console.log(err);
+            }
+        }
+        return arr;
+    }
+    useEffect(() => {
+      if(user.user && user.user.Email){
+        const db = firebase.firestore();
+      db.collection("reviews").where("uid", "==", user.user.Email)
+      .get()
+      .then(async function(querySnapshot) {
+          let arr = [];
+          arr = await getMovieDetails(querySnapshot);
+          console.log(arr);
+          setreviewDetail(arr);
+      })
+      .catch(function(error) {
+          console.log("Error getting documents: ", error);
+      });
+      }
+  },[user.isLoggedIn])
+
+  const deleteReview = async (e,reviewID,x) => {
+    console.log("DElete Review")
+    e.preventDefault();
+    const db = firebase.firestore();
+    let arr = reviewDetail.filter(element => element !== x)
+    console.log(arr);   
+    db.collection("reviews").doc(reviewID).delete().then(function() {
+        setreviewDetail(arr);
+  }).catch(function(error) {
+      console.error("Error removing document: ", error);
+  });
+  }
   let nominations;
   if(user.isLoggedIn)
   {
@@ -135,7 +199,7 @@ function DashboardPageComponent(){
      }
   },[user.isLoggedIn])
   // Main Return from this component
-  if(user.isLoading)
+  if(user.isLoading || !user.isLoggedIn)
     return(
       <CircularProgress style={{marginTop:"25vw"}} color="secondary" ></CircularProgress>
     )
@@ -177,6 +241,67 @@ function DashboardPageComponent(){
             :
             <div></div>
             }
+                    <div>
+        <Card className={classes.root} style={{marginTop:"10vh"}}>
+            <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                image={user.user.ProfilePic}
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {user.user.Name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {user.user.Email}
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Share
+                </Button>
+                <Button size="small" color="primary">
+                Learn More
+                </Button>
+            </CardActions>
+            </Card>
+            {
+                (reviewDetail.length) ?
+                (
+                    reviewDetail.map((x) => {
+                        return(
+                            <Card className={classes.root} style={{marginTop:"5vh"}}>
+            <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                image={`https://image.tmdb.org/t/p/w500${x.backdrop_path}`}
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {x.review}
+                </Typography>
+                <Rating precision="0.5" value={x.rating} readOnly/>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                Edit
+                </Button>
+                <Button size="small" color="primary" onClick={(e)=>{ deleteReview(e,x.reviewID,x); }}>
+                Delete
+                </Button>
+            </CardActions>
+            </Card>
+                        )
+                    })
+                )
+                :
+                <div>
+                    Nothing Here
+                </div>
+            }
+        </div>
           </main>
         </React.Fragment>
         );
